5fedffca773e65ce676770d9bcd09331863c27f1
Mateus Gunsch <mateusgunsch01@gmail.com>
2024-11-13 08:52:18 -0400

    Local storage data saving added

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const inputBox = document.getElementById("inputBox")
 const listContainer = document.getElementById("listContainer")
 
-function addTask(){
+function addTask() {
     if(inputBox.value === '') {
         alert("You must write something!")
     } else {
@@ -13,13 +13,25 @@ function addTask(){
         li.appendChild(span)
     }
     inputBox.value = ''
+    saveData()
 }
 
 listContainer.addEventListener("click", function(ev) {
     if(ev.target.tagName === "LI") {
         ev.target.classList.toggle("checked")
+        saveData()
     }
     else if(ev.target.tagName === "SPAN") {
         ev.target.parentElement.remove()
+        saveData()
     }
 }, false)
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML)
+}
+
+function showSavedTask() {
+    listContainer.innerHTML = localStorage.getItem("data")
+}
+showSavedTask()
